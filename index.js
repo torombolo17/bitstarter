8b21b23016d3c182edae4ac076038bd8a3d71726
Ubuntu <ubuntu@ip-172-31-21-189.us-west-2.compute.internal>
2014-11-03 00:48:29 +0000

    changed index.html and index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+var fs = require('fs');
+
+var buf = fs.readFileSync('index.html');
+buf = buf.toString()
 var express = require('express')
 var app = express();
-var fs = require('fs')
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT ||8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html", 'utf-8'))
+  response.send(buf)
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
